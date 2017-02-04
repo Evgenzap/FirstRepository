13dc78ca55bf7681dc2ddd04f7d249002d40261a
Evgenzap <evgenzap1337@gmail.com>
2017-02-04 21:54:13 +0300

    Add files via upload

---
+++ psp4/src/test/resources/test.sql
@@ -0,0 +1,4 @@
+insert into students(id, name, univercity, cource) values('3', 'zzz', 'bsuir', '5');
+#delete from students where id=1;
+select * from students;
+#update students set name='lol', univercity='qqq' where id=1;
